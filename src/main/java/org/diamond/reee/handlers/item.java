ba88e4fadc1b6709492f7793132d2694a8e3824b
willa <willaimj@gmail.com>
2023-10-09 20:36:44 -0400

    Yee

--- src/main/java/org/diamond/reee/handlers/item.java
+++ src/main/java/org/diamond/reee/handlers/item.java
@@ -3,6 +3,11 @@ package org.diamond.reee.handlers;
 import org.bukkit.ChatColor;
 
 public class item {
+    /**
+     * Translate normal text to colored text
+     * @param msg String of text to translate using the & sign
+     * @return String
+     */
     public static String test(String msg){
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
